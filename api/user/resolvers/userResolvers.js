3eb0097e56c930000be1c76e732194fedcb8f108
Vinicius Imai <vinicius.imai@gmail.com>
2021-03-14 20:43:01 -0300

    feat(users): adicionando novo campo createdAt

--- api/user/resolvers/userResolvers.js
+++ api/user/resolvers/userResolvers.js
@@ -1,6 +1,14 @@
+const { GraphQLScalarType } = require('graphql')
 
 
 const userResolvers = {
+    DateTime: new GraphQLScalarType({
+      name: 'DateTime',
+      description: 'string dfe data e hora ISO-8601',
+      serialize: (value) => value.toISOString(),
+      parseValue: (value) => new Date(value),
+      parseLiteral: (ast) => new Date(ast.value)
+    }),
     Query: {
         users: (root, args, { dataSources }) => dataSources.usersAPI.getUsers(),
         user: (root, { id }, { dataSources }) => dataSources.usersAPI.getUserById(id),
